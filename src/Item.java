e6cd23dc1e73c0eca4617c055b7068fcbea24da8
Erick Otsuka <erick.r.otsuka@gmail.com>
2017-05-04 11:03:16 -0300

    Adiciona classe Item

---
+++ src/Item.java
@@ -0,0 +1,11 @@
+
+public class Item {
+	private double cura;
+	private static final int prioridade = 5;
+	public double getCura() {
+		return this.cura;
+	}
+	public int getPrioridade() {
+		return this.prioridade;
+	}
+}
