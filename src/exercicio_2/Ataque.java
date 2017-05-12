43a3a5973195d0c8cb0692463f109f5b15216b47
Erick Otsuka <erick.r.otsuka@gmail.com>
2017-05-12 14:38:02 -0300

    Copia classes para pacote exercicio_2

---
+++ src/exercicio_2/Ataque.java
@@ -0,0 +1,21 @@
+package exercicio_2;
+
+public class Ataque {
+	private String nome;
+	private double dano;
+	private int prioridade;
+	public Ataque (String nome, double dano, int prioridade) {
+		this.nome = nome;
+		this.dano = dano;
+		this.prioridade = prioridade; //ataque de prioridade 5 ataca antes do que o de 4
+	}
+	public double getDano () {
+		return dano;
+	}
+	public int getPrioridade () {
+		return prioridade;
+	}
+	public String getNome() {
+		return nome;
+	}
+}
---
+++ src/exercicio_2/Evento.java
@@ -0,0 +1,10 @@
+package exercicio_2;
+
+abstract public class Evento {
+	abstract public void acao();
+	abstract public String descricao();
+	abstract public int getPrioridade();
+	abstract public Trainer getAtacante();
+	abstract public Trainer getDefesa();
+	abstract public int getA();
+}
